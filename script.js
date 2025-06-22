37d57b6a81506d9ee8f253503f9b5e2b316c516a
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-22 08:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+const ip = document.getElementById("ip");
+const output = document.getElementById("output");
+
+btn.addEventListener("click", handleSubmit);
+
+async function handleSubmit() {
+  const ipValue = ip.value.trim();
+  let currValue = await firstPromise(ipValue);
+  currValue = await secondPromise(currValue);
+  currValue = await thirdPromise(currValue);
+  currValue = await fourthPromise(currValue);
+  currValue = await fifthPromise(currValue);
+}
+
+async function firstPromise(ip) {
+  const newIp = ip;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("First promise resolved with:", newIp);
+      output.innerText = `Result: ${newIp}`;
+      resolve(newIp);  // pass the value forward
+    }, 2000);
+  });
+}
+
+async function secondPromise(ip) {
+  const newIp = ip * 2;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Second promise resolved with:", newIp);
+      output.innerText = `Result: ${newIp}`;
+      resolve(newIp);  // pass the value forward
+    }, 2000);
+  });
+}
+
+async function thirdPromise(ip) {
+  const newIp = ip - 3;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Third promise resolved with:", newIp);
+      output.innerText = `Result: ${newIp}`;
+      resolve(newIp);  // pass the value forward
+    }, 1000);
+  });
+}
+
+async function fourthPromise(ip) {
+  const newIp = ip/2;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Fourth promise resolved with:", newIp);
+      output.innerText = `Result: ${newIp}`;
+      resolve(newIp);  // pass the value forward
+    }, 1000);
+  });
+}
+async function fifthPromise(ip) {
+  const newIp = ip + 10
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Fifth promise resolved with:", newIp);
+      output.innerText = `Final Result: ${newIp}`;
+      resolve(newIp);  // pass the value forward
+    }, 1000);
+  });
+}
